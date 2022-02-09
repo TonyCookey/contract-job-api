ba7228ddfd7556c64444cdf406a1f5b73a97127e
Anthony Iroegbu <anthony@crenettechlabs.com>
2022-02-09 16:23:45 +0100

    updated tests for contracts controller

--- src/controllers/ContractController.js
+++ src/controllers/ContractController.js
@@ -5,22 +5,30 @@ const { Op } = require("sequelize");
  * @returns contract by id
  */
 async function getContractForAuthProfile(req, res) {
-    const { Contract } = req.app.get('models')
+    try {
+        const { Contract } = req.app.get('models')
 
-    const { id } = req.params
-    const contract = await Contract.findOne({
-        where: {
-            id,
-            [Op.or]: [
-                { ClientId: req.profile.id },
-                { ContractorId: req.profile.id }
-            ]
-        }
-    })
-    if (!contract) return res.status(404).json({
-        message: 'Could not find requested contract'
-    }).end()
-    return res.status(200).json(contract)
+        const { id } = req.params
+        const contract = await Contract.findOne({
+            where: {
+                id,
+                [Op.or]: [
+                    { ClientId: req.profile.id },
+                    { ContractorId: req.profile.id }
+                ]
+            }
+        })
+        if (!contract) return res.status(404).json({
+            message: 'Could not find requested contract'
+        }).end()
+        return res.status(200).json(contract)
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({
+            message: 'System encountered an error',
+            error
+        })
+    }
 }
 
 /**
@@ -28,25 +36,39 @@ async function getContractForAuthProfile(req, res) {
  * @returns contract by id
  */
 async function getNonTerminatedContractForAuthProfile(req, res) {
-    const { Contract } = req.app.get('models')
+    try {
+        const { Contract } = req.app.get('models')
 
-    const contracts = await Contract.findAll({
-        where: {
-            status: {
+        const contracts = await Contract.findAll({
+            where: {
+                status: {
+                    [Op.or]: [
+                        'new', 'in_progress'
+                    ]
+                },
                 [Op.or]: [
-                    'new', 'in_progress'
+                    { ClientId: req.profile.id },
+                    { ContractorId: req.profile.id }
                 ]
-            },
-            [Op.or]: [
-                { ClientId: req.profile.id },
-                { ContractorId: req.profile.id }
-            ]
-        }
-    })
-    if (!contracts) return res.status(204).json({
-        message: 'Could not find non terminated contracts - new or in_progress contracts'
-    }).end()
-    return res.status(200).json(contracts)
+            }
+        })
+
+        if (!contracts || contracts.length == 0) return res.status(200).json({
+            message: 'Could not find non terminated contracts - new or in_progress contracts',
+            result: contracts
+        }).end()
+
+        return res.status(200).json({
+            message: 'successfully returned all unterminated contracts for this profile',
+            result: contracts
+        })
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({
+            message: 'System encountered an error',
+            error
+        })
+    }
 }
 module.exports = {
     getContractForAuthProfile,
--- src/tests/contracts.spec.js
+++ src/tests/contracts.spec.js
@@ -4,20 +4,28 @@ const request = require('supertest');
 const app = require('../app')
 
 const authProfile = {
-    id: 1,
-    firstName: 'Harry',
-    lastName: 'Potter',
-    profession: 'Wizard',
-    balance: 1150,
+    id: 4,
+    firstName: 'Ash',
+    lastName: 'Kethcum',
+    profession: 'Pokemon master',
+    balance: 1.3,
     type: 'client'
 }
 
 const authProfileContract = {
-    id: 1,
+    id: 7,
     terms: 'bla bla bla',
-    status: 'terminated',
-    ClientId: 1,
-    ContractorId: 5
+    status: 'in_progress',
+    ClientId: 4,
+    ContractorId: 7
+}
+const NoContractProfile = {
+    id: 5,
+    firstName: 'John',
+    lastName: 'Lenon',
+    profession: 'Musician',
+    balance: 64,
+    type: 'contractor'
 }
 const sampleContract = {
     id: 3,
@@ -26,10 +34,10 @@ const sampleContract = {
     ClientId: 2,
     ContractorId: 6
 }
-describe('TEST - /contract/:id route ', () => {
+describe('TEST - Fetch Contracts Controller Endpoint - /contracts/:id', () => {
 
-    describe('TEST - Fetch a single contract for an authenticated Profile ', () => {
-        test('It should respond with 200 success', async () => {
+    describe('PASS - Fetch a single contract for an authenticated Profile ', () => {
+        test('It should respond with 200 OK', async () => {
             const response = await request(app)
                 .get(`/contracts/${authProfileContract.id}`)
                 .set('profile_id', authProfile.id)
@@ -38,16 +46,17 @@ describe('TEST - /contract/:id route ', () => {
         });
     });
 
-    describe('TEST - Fetch a single contract for an unathenticated Profile ', () => {
+    describe('FAIL - Fetch a single contract for an unathenticated Profile ', () => {
         test('It should respond with 401 Unauthorized', async () => {
             const response = await request(app)
                 .get(`/contracts/${authProfileContract.id}`)
                 .expect('Content-Type', /json/)
                 .expect(401);
         });
+
     });
 
-    describe('TEST - Fetch a single contract that does not exist ', () => {
+    describe('FAIL - Fetch a single contract that does not exist ', () => {
         test('It should respond with 404 Not Found', async () => {
             const response = await request(app)
                 .get(`/contracts/500`)
@@ -56,7 +65,7 @@ describe('TEST - /contract/:id route ', () => {
                 .expect(404);
         });
     });
-    describe('TEST - Fetch a single contract that does not belong to the authenticated user ', () => {
+    describe('FAIL - Fetch a single contract that does not belong to the authenticated user ', () => {
         test('It should respond with 404 Not Found', async () => {
             const response = await request(app)
                 .get(`/contracts/${sampleContract.id}`)
@@ -65,4 +74,39 @@ describe('TEST - /contract/:id route ', () => {
                 .expect(404);
         });
     });
+})
+// testing the /contracts endpoint
+describe('TEST - Fetch Contracts Controller Endpoint - /contracts/', () => {
+    describe('PASS - Fetch all unterminated contracts for an authenticated Profile ', () => {
+        test('It should respond with 200 OK', async () => {
+            const response = await request(app)
+                .get(`/contracts`)
+                .set('profile_id', authProfile.id)
+                .expect('Content-Type', /json/)
+                .expect(200);
+
+            expect(response.body.message).toBe('successfully returned all unterminated contracts for this profile');
+            expect(response.body.result).not.toHaveLength(0);
+            expect(response.body.result.length).toBeGreaterThan(0);
+        });
+        test('It should respond with 200 OK, but with no contracts found', async () => {
+            const response = await request(app)
+                .get(`/contracts`)
+                .set('profile_id', NoContractProfile.id)
+                .expect('Content-Type', /json/)
+                .expect(200)
+            expect(response.body.message).toBe('Could not find non terminated contracts - new or in_progress contracts');
+            expect(response.body.result).toHaveLength(0);
+        });
+    });
+
+    describe('FAIL - Fetch all unterminated contracts for an unathenticated Profile ', () => {
+        test('It should respond with 401 Unauthorized', async () => {
+            const response = await request(app)
+                .get(`/contracts`)
+                .expect('Content-Type', /json/)
+                .expect(401);
+        });
+    });
+
 })
