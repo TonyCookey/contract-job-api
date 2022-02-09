7bc27da990e893c2798484cce8e0149e58f36aea
Anthony Iroegbu <anthony@crenettechlabs.com>
2022-02-09 13:24:41 +0100

    wrote tests for general API ping

---
+++ src/tests/api.spec.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+
+const app = require('../app')
+
+describe('Test API Server is alive and responding ', () => {
+    test('It should respond with 200 success', async () => {
+        const response = await request(app)
+            .get('/')
+            .expect('Content-Type', /json/)
+            .expect(200);
+    });
+});
