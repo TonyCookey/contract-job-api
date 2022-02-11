127356a501e0dd4636032f0b40a9ea9c756c1d0b
Anthony Iroegbu <anthony@crenettechlabs.com>
2022-02-11 04:41:44 +0100

    created admin controller to handle admin routes

---
+++ src/controllers/AdminController.js
@@ -0,0 +1,17 @@
+const { Op } = require("sequelize");
+
+/**
+ * get the highest earning profession during a time range
+ */
+async function getHighestEarningrofession(req, res) { }
+
+
+/**
+ * get the highest paying client during a time range
+ */
+async function getHighestPayingClient(req, res) { }
+
+module.exports = {
+    getHighestEarningrofession,
+    getHighestPayingClient
+}
